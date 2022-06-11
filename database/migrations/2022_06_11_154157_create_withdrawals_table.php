6cd80bac747c47f5c442fd58ff593a251967c946
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2022-06-11 19:02:12 +0200

    withdrawal and payouts table

---
+++ database/migrations/2022_06_11_154157_create_withdrawals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWithdrawalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('withdrawals', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('marketer_id');
+            $table->text('method');
+            $table->text('amount');
+            $table->date('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('withdrawals');
+    }
+}
---
+++ database/migrations/2022_06_11_154423_create_payout_settings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePayoutSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payout_settings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('marketer_id');
+            $table->text('method');
+            $table->text('paypal_account')->nullable();
+            $table->text('bank_name')->nullable();
+            $table->text('bank_account')->nullable();
+            $table->text('bank_account_number')->nullable();
+            $table->text('id_number')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payout_settings');
+    }
+}
